c7a2267f99c567d187dc2df4beee33816de42e73
maxweil <maxweil@notabug.org>
2021-08-18 21:13:42 -0500

    Clean up javascript to be async and add album list

---
+++ radioListHelper.js
@@ -0,0 +1,35 @@
+const fs = require('fs')
+const LIST_PATH = 'public/radiolist.txt'
+
+function buildRadioList()
+{
+	try {
+		var data = fs.readFileSync(LIST_PATH, 'utf8');
+	}
+	catch
+	{
+		return [];
+	}
+
+	data = data.split("\n")
+	if (data.length <= 1)
+	{
+		return [];
+	}
+
+	var radiolist = [];
+	radiolist.date=data[0];
+
+	for (var i = 1; i < data.length; i++)
+	{
+		album = {};
+		album.title=data[i];
+		if (album.title) 
+		{
+			radiolist.push(album)
+		}
+	}
+	return radiolist.reverse()
+}
+
+exports.buildRadioList = buildRadioList;
